1112d42cf1c7dc42b5e0d10c2fa814134ffdc4b6
Paula <paula.isaza@knmi.nl>
2020-09-20 21:32:50 +0200

    Simple controller test

--- src/test/java/nl/isaza/rabo/assignment/api/UserControllerTest.java
+++ src/test/java/nl/isaza/rabo/assignment/api/UserControllerTest.java
@@ -1,12 +1,32 @@
 package nl.isaza.rabo.assignment.api;
 
 import nl.isaza.rabo.assignment.AssignmentApplication;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {AssignmentApplication.class})
+@SpringBootTest(
+        classes = {AssignmentApplication.class},
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@AutoConfigureWebTestClient
 class UserControllerTest {
+    private static final String USER_PATH = "/users";
+    private static final String FIRSTNAME = "Jane";
+    private static final String SURNAME = "Doe";
+    @Autowired
+    private WebTestClient api;
 
+    @Test
+    public void testGetUser() {
+        api.get().uri(USER_PATH)
+                .exchange()
+                .expectStatus().isOk();
+
+    }
 }
