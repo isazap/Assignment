064b96930dbb84a47e9c9667b69c624f71d07cf2
Paula <paula.isaza@knmi.nl>
2020-09-20 15:49:15 +0200

    Initialize project from spring boot starter

---
+++ src/test/java/test/isaza/rabo/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package test.isaza.rabo.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
