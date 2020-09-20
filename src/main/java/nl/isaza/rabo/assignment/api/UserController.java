4304cdec698316e9b8fcb191200d2eb2d24acb56
Paula <paula.isaza@knmi.nl>
2020-09-20 20:36:44 +0200

    Wire in service to get external user data

--- src/main/java/nl/isaza/rabo/assignment/api/UserController.java
+++ src/main/java/nl/isaza/rabo/assignment/api/UserController.java
@@ -1,6 +1,6 @@
 package nl.isaza.rabo.assignment.api;
 
-import nl.isaza.rabo.assignment.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    UserService userService;
+
     @GetMapping
     public UserDto getUser(@PathVariable String first, @PathVariable String surname) {
-        //service to get the user with this first name and surname
-        return new UserDto();
+        return userService.findUser(first, surname);
     }
 }
