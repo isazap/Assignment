8676f7710a24bb426abc67b33c88914bc0f49ee1
Paula <paula_turnbull@hotmail.com>
2020-09-22 15:20:28 +0200

    Setting up config for xapi

---
+++ xuser-api-client/src/main/java/nl/isaza/rabo/xapi/http/XUserApiConfiguration.java
@@ -0,0 +1,14 @@
+package nl.isaza.rabo.xapi.http;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "xuser")
+public class XUserApiConfiguration {
+    private String baseUrl;
+}
